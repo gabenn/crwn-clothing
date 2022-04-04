2a2274fc40d34b5a18c8e2f45e22ee4ba897a143
Lukasz <Lukaszcysewski123@gmail.com>
2022-04-04 20:33:02 +0200

    Routing react-router v6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from './routes/homepage/homepage.component';
+import Navigation from './routes/navigation/navigation.component';
+import { Routes, Route } from 'react-router-dom';
+import SignIn from './routes/sign-in/SignIn.component';
+
+const ShopPage = () => {
+  return ( 
+    <div>
+      shopPage
+    </div> );
+}
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Routes>
+        <Route path='/' element={<Navigation/>}>
+          <Route index element={<HomePage/>}/>
+          <Route path='shop' element={<ShopPage/>}/>
+          <Route path='sign-in' element={<SignIn/>}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/routes/homepage/homepage.component.jsx
+++ src/routes/homepage/homepage.component.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
+import { Outlet } from 'react-router-dom';
+
 const HomePage = () => {
   return ( 
     <div className='homepage'>
       <Directory/>
+      <Outlet/>
     </div> );
 }
  
