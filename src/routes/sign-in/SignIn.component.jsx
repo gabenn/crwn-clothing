08b08b901e6ac14a5e50b6d1807445f423bcb0dc
Lukasz <Lukaszcysewski123@gmail.com>
2022-05-12 22:09:52 +0200

    other signin method

--- src/routes/sign-in/SignIn.component.jsx
+++ src/routes/sign-in/SignIn.component.jsx
@@ -1,12 +1,28 @@
 import './SignIn.styles.scss'
-import {signInWithGooglePopup, createUserDocumentFromAuth} from '../../utils/firebase/firebase.util.js';
+import {
+  signInWithGooglePopup, 
+  createUserDocumentFromAuth,
+  signInWithGoogleRedirect, 
+  auth
+} from '../../utils/firebase/firebase.util.js';
+// import { useEffect } from 'react';
+// import { getRedirectResult } from 'firebase/auth';
 
 const SignIn = () => {
+
+  // useEffect(async () => {
+  //   const response = await getRedirectResult(auth)
+  //   if(response){
+  //     const userDocRef = await createUserDocumentFromAuth(response.user);
+  //   }
+  // },[])
 
   const logGoogleUser = async () => {
     const {user} = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   }
+
+  
 
   return ( 
     <div>
@@ -14,6 +30,9 @@ const SignIn = () => {
       <button onClick={logGoogleUser}>
         Sign In With Google Popup
       </button>
+      {/* <button onClick={signInWithGoogleRedirect}>
+        Sign In With Google Redirect
+      </button> */}
     </div>
    );
 }
